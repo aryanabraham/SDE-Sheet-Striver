58ce80c83ec6c6e5b29e841b7347f216b4b9d0a4
Aryan Abraham <abraham.aryan01@gmail.com>
2023-06-27 12:47:46 +0530

    Create Height of a Binary Tree.cpp

---
+++ Binary Tree Part-II/Height of a Binary Tree.cpp
@@ -0,0 +1,10 @@
+int maxDepth(TreeNode* root) {
+        if(root == NULL){
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root->left);
+        int rightDepth = maxDepth(root->right);
+
+        return (1 + max(leftDepth, rightDepth));
+    }

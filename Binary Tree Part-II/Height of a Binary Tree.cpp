8c8c3f4eb8f8bef6cde4a870441ff8b44ba4ddc2
Aryan Abraham <abraham.aryan01@gmail.com>
2023-07-13 17:02:07 +0530

    Update Height of a Binary Tree.cpp

--- Binary Tree Part-II/Height of a Binary Tree.cpp
+++ Binary Tree Part-II/Height of a Binary Tree.cpp
@@ -1,10 +1,32 @@
-int maxDepth(TreeNode* root) {
-        if(root == NULL){
-            return 0;
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T val;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
         }
+    };
 
-        int leftDepth = maxDepth(root->left);
-        int rightDepth = maxDepth(root->right);
+************************************************************/
 
-        return (1 + max(leftDepth, rightDepth));
+int maxDepth(TreeNode<int> *root) {
+    // Write your code here.
+    if(root == NULL){
+        return -1;
     }
+
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return (1 + max(leftDepth, rightDepth));
+}

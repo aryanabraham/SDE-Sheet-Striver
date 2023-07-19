52d66fae09fca6a6f84059c914d35754263e4585
Aryan Abraham <abraham.aryan01@gmail.com>
2023-07-19 12:36:23 +0530

    Update BFS.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,1 +1,24 @@
-
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        queue<int> q;
+        vector<int> bfsNodes;
+        vector<int> visited(V, 0);
+        
+        visited[0] = 1;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            bfsNodes.push_back(node);
+            
+            for(auto &it: adj[node]){
+                if(!visited[it]){
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfsNodes;
+    }
